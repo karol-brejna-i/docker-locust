27679e02457535342d09fd0e23a9e68a7cb75120
Benoit Martin <metabsd@users.noreply.github.com>
2020-02-07 09:14:50 -0500

    Update docker-entrypoint.sh -- shell best practice (#13)

--- 0.13.5/python3.8/alpine3.11/docker-entrypoint.sh
+++ 0.13.5/python3.8/alpine3.11/docker-entrypoint.sh
@@ -24,7 +24,7 @@ fi
 
 LOCUST_OPTS="-f ${LOCUST_FILE} --host=${ATTACKED_HOST} $LOCUST_OPTS"
 
-case `echo ${LOCUST_MODE} | tr 'a-z' 'A-Z'` in
+case $(echo "${LOCUST_MODE}" | tr '[:lower:]' '[:upper:]') in
 "MASTER")
     LOCUST_OPTS="--master --master-bind-port=${LOCUST_MASTER_BIND_PORT} $LOCUST_OPTS"
     ;;
