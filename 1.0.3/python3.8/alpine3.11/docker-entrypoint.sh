ab47fbba78368377c5ab33b965cbeb6ba63e0237
youngkih <youngkih21@gmail.com>
2020-07-11 16:19:13 +0900

    Update copyright

--- 1.0.3/python3.8/alpine3.11/docker-entrypoint.sh
+++ 1.0.3/python3.8/alpine3.11/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#   Copyright 2020 Youngki Hong
+#   Copyright 2019 Karol Brejna
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
